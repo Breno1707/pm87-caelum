2bef47370553f72bd92c8eeb0aa6af801e885404
Breno Piloto <brenopiloto@hubert.com.br>
2017-12-12 16:30:12 -0200

    primeiro commit do projeto com classe de exemplo

---
+++ pm-caelum/pm-caelum/Teste.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace pm_caelum
+{
+    class Teste
+    {
+        public static void main(string[] args)
+        {
+            Console.WriteLine("Exemplo de teste de uso do git");
+        }
+    }
+}
